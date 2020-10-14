0fd6526d9d3f0666569c8bff62b0de9a33fc016e
Andy <moreno.dosso.andres@gmail.com>
2020-10-14 10:49:15 +0200

    Lecture 39

--- src/main/java/andy/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/andy/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -2,9 +2,10 @@ package andy.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ES")
+
+@Profile({"ES", "default"})
 @Service("i18nService")
-public class I18nSpanishGreetingService implements GreetingService{
+public class I18nSpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
         return "Hola Mundo - ES";
