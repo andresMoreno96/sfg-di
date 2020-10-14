17db754d7cb89349a2e55851543c1da1916d3032
Andy <moreno.dosso.andres@gmail.com>
2020-10-14 10:02:19 +0200

    Lecture 38

--- src/main/java/andy/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/andy/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package andy.springframework.sfgdi;
 
-import andy.springframework.sfgdi.controllers.ConstructorInjectionController;
-import andy.springframework.sfgdi.controllers.MyController;
-import andy.springframework.sfgdi.controllers.PropertyInjectionController;
-import andy.springframework.sfgdi.controllers.SetterInjectionController;
+import andy.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayGreeting());
+
 
         MyController myController = (MyController) ctx.getBean("myController");
         System.out.println("----Primary Bean----");
